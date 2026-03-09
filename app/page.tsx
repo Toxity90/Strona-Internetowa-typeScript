a1d0a0cb3630571fe9ee0c3ac01c11e0353be595
Michal <bliznia2009@gmail.com>
2026-03-09 12:10:37 +0100

    test zmiany

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Search from "@/components/Search"; // 1. IMPORTUJESZ SWÓJ KOMPONENT z pl
 export default function Home() { // Tworzysz główną funkcję (komponent) dla swojej strony. export pozwala użyć go w innych częściach strony, ale tu jest to główny komponent, więc będzie wyświetlany jako strona główna.
   return ( // To jest JSX, czyli specjalny język, który pozwala Ci pisać kod interfejsu użytkownika w JavaScript. Wszystko, co jest pod tym słowem, zostanie narysowane na ekranie.
     <main>
-      <h1>Co masz w lodówce 🍎</h1> 
+      <h1>Co masz w lodówce www 🍎</h1> 
 
       <Search /> 
       
